ae5a8140474132de1768191608f79444047c13a6
Riccardo <riccardo.lamalfa94@gmail.com>
2024-01-23 11:26:51 +0100

    rimosso eval() (push precedente non valido)

--- src/App.js
+++ src/App.js
@@ -3,35 +3,45 @@ import InputArea from './components/InputArea';
 import { useState } from 'react';
 
 function App() {
-  let result = 0;
-  const position = 'inputArea';
-  const [inputs, setInputs] = useState([]);
+  const [result, setResult] = useState(0);
+  const [inputs, setInputs] = useState([true, true, true]);
+  const symbols = ['+', '-'];
+  let total = 0
+
+  function calculateResult() {
+    let inputs = document.getElementsByClassName("input-number");
+    Object.values(inputs).forEach(number => {
+      if (number.disabled) { return null }
+      const value = Number(number.value);
+      let symbol = number.parentNode.childNodes[0].selectedOptions[0].value;
+      total = symbol === symbols[0] ? total + value :
+        symbol === symbols[1] ? total - value : 0;
+    });
+    setResult(total);
+  }
 
   function addInput() {
     setInputs([...inputs, true]);
   }
 
   return (
-    <div className='vw-100 vh-min-100 bg-light'>
+    <div className='vw-100 vh-100 bg-light'>
       <div className="App w-100 w-md-75 mx-auto container">
 
-        <h1 className='pt-5'>REACT Components Version</h1>
+        <h1 className='pt-5'>REACT Simple Calculator</h1>
 
         <button className='btn btn-primary my-4 w-auto rounded-5 p-2 px-4' onClick={addInput}>Add row</button>
 
         <div className='row justify-content-center'>
           <div className='row row-gap-3 m-1 border border-1 rounded-5 p-5 w-75' id='numberContainer' inputs={inputs}>
-            <InputArea position={position} />
-            <InputArea position={position} />
-            <InputArea position={position} />
-            {inputs.map((element, index) => {
-              return (<InputArea position={position} key={index} />)
+            {inputs.map((x, index) => {
+              return (<InputArea position='inputArea' calculate={() => calculateResult()} key={index} />)
             })}
           </div>
         </div>
 
         <div className='row py-5 justify-content-center'>
-          <h5 className='bg-dark w-auto rounded-5 p-3 text-light px-4'>Result: <span id="result" className='h4'>0</span></h5>
+          <h5 className='bg-dark w-auto rounded-5 p-3 text-light px-4'>Result: {result}</h5>
         </div>
       </div>
     </div>
--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -6,25 +6,24 @@ import InputButton from "./InputButton";
 export default function InputArea(props) {
     const symbols = ['+', '-'];
 
-    const calculateResult = () => {
+    /* function calculateResult (){
+        let total = 0;
         let inputs = document.getElementsByClassName("input-number");
-        let result = 0;
-        Object.values(inputs).forEach(element => {
-            let symbol = element.parentNode.childNodes[0].selectedOptions[0].value;
-            if (element.valueAsNumber && !element.disabled) {
-                result += symbol;
-                result += element.valueAsNumber;
-            }
-        })
-        const resultText = document.getElementById("result");
-        resultText.textContent = Number(eval(result)) ? eval(result) : "0";
-    }
+        Object.values(inputs).forEach(number => {
+            if(number.disabled){return null}
+            const value = Number(number.value);
+            let symbol = number.parentNode.childNodes[0].selectedOptions[0].value;
+            total = symbol===symbols[0]?total+value:
+            symbol===symbols[1]?total-value:0;
+        });
+        // result(total);
+    } */
 
     /* Delete button */
     const deleteInput = (e) => {
         let element = e.target.parentNode;
         element.parentNode.removeChild(element);
-        calculateResult();
+        props.calculate();
     }
 
     /* Enable button */
@@ -33,13 +32,13 @@ export default function InputArea(props) {
         setEnable(enable === "Disable" ? "Enable" : "Disable");
         let selectedInput = e.target.parentNode.childNodes[1];
         selectedInput.disabled = !selectedInput.disabled;
-        calculateResult();
+        props.calculate();
     };
 
     return (
         <div className="row column-gap-3 justify-content-center my-2">
-            <ListOperator clickFunction={calculateResult} symbols={symbols} />
-            <InputValue calculateResult={calculateResult} />
+            <ListOperator clickFunction={props.calculate} symbols={symbols} />
+            <InputValue calculateResult={props.calculate} />
             <InputButton value={"Delete"} clickFunction={deleteInput} class={"btn col rounded-5 btn-danger"} />
             <InputButton value={enable} clickFunction={toggleEnable} class={"btn col rounded-5 btn-primary"} />
         </div>
